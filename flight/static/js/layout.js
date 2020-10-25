5fe23a0be29b75de1df7ce51933737e00c1cfa08
Ayush Kumar <shop.ayush930@gmail.com>
2020-10-25 17:58:08 +0530

    Half done index_page->city_choose

--- flight/static/js/layout.js
+++ flight/static/js/layout.js
@@ -1,34 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(".dropdown-input").forEach(dropdown => {
-        dropdown.querySelectorAll("a").forEach(option => {
-            option.onclick = () => {
-                //
-            }
-        });
-    });
-});
+//function esc(element) {
+//    document.addEventListener('keydown', event => {
+//        if(event.key === 'Escape') {
+//            element.style.display = 'none';
+//        }
+//    });
+//    element.parentElement.querySelector('input[type=text]').addEventListener("blur", () => {
+//        setTimeout(() => {
+//            element.style.display = 'none';
+//        },80);
+//    });
+//}
 
-function places_from() {
-    let element = document.querySelector('.places_from');
-    element.style.display = 'block';
-    esc(element);
-}
-
-function places_to() {
-    let element = document.querySelector('.places_to');
-    element.style.display = 'block';
-    esc(element);
-}
-
-function esc(element) {
-    document.addEventListener('keydown', event => {
-        if(event.key === 'Escape') {
-            element.style.display = 'none';
-        }
-    });
-    element.parentElement.querySelector('input[type=text]').addEventListener("blur", () => {
-        setTimeout(() => {
-            element.style.display = 'none';
-        },80);
-    });
-}
